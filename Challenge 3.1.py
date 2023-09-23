473c766afb9bd62d8e311ed97ac2808ffe4d57d0
Sarmila.v <144471205+Githubthamiza@users.noreply.github.com>
2023-09-23 21:17:27 +0530

    Create Challenge 3.1.py

---
+++ Challenge 3.1.py
@@ -0,0 +1,11 @@
+def LinearSearchProduct(productList,targetproduct);
+Indices -[]
+for index,product in enumanate (productlist);
+if product==Target product;
+indices.append(index)
+return indices
+products =["shoes","boat",loafer","shoes ","sandal ""shoes "]
+target="shoes "
+Target 2='apple '
+result=linearsearchproduct(products, Target 2)
+print (result)

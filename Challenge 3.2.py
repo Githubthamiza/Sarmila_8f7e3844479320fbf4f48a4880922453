3379c1555c1bccb38d29bd9d210e7fb5af44cff4
Sarmila.v <144471205+Githubthamiza@users.noreply.github.com>
2023-09-23 21:43:46 +0530

    Create Challenge 3.2.py

---
+++ Challenge 3.2.py
@@ -0,0 +1,14 @@
+class student;
+def__init__(self,nama,roll_number,cgpa);
+self.name=name
+self.roll_number=roll_number
+self.cgpa=cgpa
+def sort_student(student _list);
+sorted _students=sorted (student_list,key=iambda student:student.cgpa,reverse=true)
+return sorted _students
+student=[student["hari","A123",7.8),
+student ("saumya",A125",9.1),
+student ("mahidhar","A126",9.9)]
+sorted _students=sort _students(students)
+for student in sorted _students;
+print ("name:{},Roll number:{},CGPA:{}. format (student.name, student.roll_number,stud.cgpa))
